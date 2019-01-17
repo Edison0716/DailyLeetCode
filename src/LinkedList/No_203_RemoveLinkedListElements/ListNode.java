7efa3a9b6ba19033800e0c1d4d64eb6c9ab001f7
mac <18644056528@163.com>
2019-01-17 15:55:55 +0800

    Initial commit

---
+++ src/LinkedList/No_203_RemoveLinkedListElements/ListNode.java
@@ -0,0 +1,10 @@
+package LinkedList.No_203_RemoveLinkedListElements;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/LinkedList/No_203_RemoveLinkedListElements/Solution.java
@@ -0,0 +1,46 @@
+package LinkedList.No_203_RemoveLinkedListElements;
+
+/**
+ * FileName: Solution
+ * Author:   mac
+ * Date:     2019-01-17 15:51
+ * Description:
+ *
+ * Remove all elements from a linked list of integers that have value val.
+ * <p>
+ * Example:
+ * <p>
+ * Input:  1->2->6->3->4->5->6, val = 6
+ * Output: 1->2->3->4->5
+ *
+ * the Solution is we can create a fake header node ,so we can according to the fake node to find the previous node of current node.
+ *
+ */
+
+public class Solution {
+    public ListNode removeElements(ListNode head, int val){
+//        1、
+//        ListNode dummyHead = new ListNode(-1);
+//        dummyHead.next = head;
+//        ListNode prevNode = dummyHead;
+//        while (prevNode.next != null) {
+//            if (prevNode.next.val == val){
+//                prevNode.next = prevNode.next.next;
+//            }else {
+//                prevNode = prevNode.next;
+//            }
+//        }
+//        2、
+        if (head == null)
+            return null;
+
+        ListNode res = removeElements(head.next, val);
+
+        if (res.val == val){
+            return res;
+        }else {
+            head.next = res;
+            return head;
+        }
+    }
+}
