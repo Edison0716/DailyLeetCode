d155e69e07342a03bed1cf37396278b00434abbc
mac <18644056528@163.com>
2019-01-23 14:03:06 +0800

    -----2019/1/23----- 1、RemoveElement

--- src/Array/No_27_RemoveElement/Solution.java
+++ src/Array/No_27_RemoveElement/Solution.java
@@ -33,7 +33,7 @@ public class Solution {
     public int removeElement(int[] nums, int val) {
         int i = 0;
         for (int j = 0; j < nums.length; j++) {
-            if (nums[j] != val) {
+            if (nums[j] != val){
                 nums[i] = nums[j];
                 i++;
             }
