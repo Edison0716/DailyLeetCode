b0a1c25237c00601c351b203c92bc0ae2f467e6b
lijunlong <1056407060@qq.com>
2019-01-27 21:54:56 +0800

    -----1/27----- 1、Search Insert Position 2、Majority Sorted Array

--- src/Array/No_169_Majority_Element/Solution.java
+++ src/Array/No_169_Majority_Element/Solution.java
@@ -2,6 +2,25 @@ package Array.No_169_Majority_Element;
 
 import java.util.Arrays;
 
+/**
+ * FileName: Solution
+ * Author:
+ * Date:     2019-01-27 09:58
+ * Description: Majority_Element
+ * <p>
+ * Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+ * <p>
+ * You may assume that the array is non-empty and the majority element always exist in the array.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [3,2,3]
+ * Output: 3
+ * Example 2:
+ * <p>
+ * Input: [2,2,1,1,1,2,2]
+ * Output: 2
+ */
 public class Solution {
     public int majorityElement(int[] nums) {
 
@@ -26,7 +45,6 @@ public class Solution {
             }
         }
 
-
         return nums[index];
     }
 
@@ -40,15 +58,14 @@ public class Solution {
         int res = 0;
         int count = 0;
 
-
-        for (int num:nums){
-            if (count == 0){
+        for (int num : nums) {
+            if (count == 0) {
                 res = num;
             }
 
-            if (res!=num){
+            if (res != num) {
                 count--;
-            }else {
+            } else {
                 count++;
             }
 
