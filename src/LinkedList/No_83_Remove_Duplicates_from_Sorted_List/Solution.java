a827a01d3f4e8e181e535aa3f50e3ffd6f6ad6d7
lijunlong <1056407060@qq.com>
2019-02-06 10:51:10 +0800

    -----02/06----- 1、Remove Duplicates from Sorted List

---
+++ src/LinkedList/No_83_Remove_Duplicates_from_Sorted_List/Solution.java
@@ -0,0 +1,33 @@
+package LinkedList.No_83_Remove_Duplicates_from_Sorted_List;
+
+import LinkedList.ListNode;
+
+/**
+ * FileName: Solution
+ * Author:   Remove Duplicates from Sorted List
+ * Date:     2019-02-06 10:12
+ * Description: Remove Nth Node From End of List
+ * Given a sorted linked list, delete all duplicates such that each element appear only once.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: 1->1->2
+ * Output: 1->2
+ * Example 2:
+ * <p>
+ * Input: 1->1->2->3->3
+ * Output: 1->2->3
+ */
+
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = head;
+        while (dummy != null && dummy.next != null) {
+            if (dummy.val == dummy.next.val)
+                dummy.next = dummy.next.next;
+            else
+                dummy = dummy.next;
+        }
+        return head;
+    }
+}
