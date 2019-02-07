a84f9528cf29ab967cbc385a3f398a5f6f0aeb62
lijunlong <1056407060@qq.com>
2019-02-07 22:38:35 +0800

    -----02/07----- 1、Add Two Numbers II 2、Update a Solution for a question of Add two Numbers I

--- src/LinkedList/No_2_AddTwoNumbers/Solution.java
+++ src/LinkedList/No_2_AddTwoNumbers/Solution.java
@@ -62,21 +62,44 @@ public class Solution {
         return node;
     }
 
+//    public ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
+//        ListNode dummy = new ListNode(-1);
+//        ListNode cur = dummy;
+//        int carry = 0;
+//        while (l1 != null || l2 != null) {
+//            int d1 = l1 == null ? 0 : l1.val;
+//            int d2 = l2 == null ? 0 : l2.val;
+//            int sum = d1 + d2 + carry;
+//            carry = sum >= 10 ? 1 : 0;
+//            cur.next = new ListNode(sum % 10);
+//            cur = cur.next;
+//            if (l1 != null) l1 = l1.next;
+//            if (l2 != null) l2 = l2.next;
+//        }
+//        if (carry == 1) cur.next = new ListNode(1);
+//        return dummy.next;
+//    }
+
     public ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(-1);
-        ListNode cur = dummy;
-        int carry = 0;
+        ListNode dummyHead = new ListNode(-1);
+        ListNode cur = dummyHead;
+        int curry = 0;
         while (l1 != null || l2 != null) {
             int d1 = l1 == null ? 0 : l1.val;
             int d2 = l2 == null ? 0 : l2.val;
-            int sum = d1 + d2 + carry;
-            carry = sum >= 10 ? 1 : 0;
+            int sum = d1 + d2 + curry;
+            curry = sum >= 10 ? 1 : 0;
             cur.next = new ListNode(sum % 10);
             cur = cur.next;
+
             if (l1 != null) l1 = l1.next;
             if (l2 != null) l2 = l2.next;
+
         }
-        if (carry == 1) cur.next = new ListNode(1);
-        return dummy.next;
+
+        if (curry == 1) cur.next = new ListNode(1);
+
+        return dummyHead.next;
     }
+
 }
