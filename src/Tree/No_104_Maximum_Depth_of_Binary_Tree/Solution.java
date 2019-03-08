f89d16dc810cd39601f1f135280d83cd2ec643bd
lijunlong <1056407060@qq.com>
2019-03-08 21:34:28 +0800

    -----03/08----- 1、Average of Levels in Binary Tree

--- src/Tree/No_104_Maximum_Depth_of_Binary_Tree/Solution.java
+++ src/Tree/No_104_Maximum_Depth_of_Binary_Tree/Solution.java
@@ -18,17 +18,16 @@ import Tree.TreeNode;
  * <p>
  * Given binary tree [3,9,20,null,null,15,7],
  * <p>
- * 3
- * / \
- * 9  20
- * /  \
- * 15   7
+ *       3
+ *      / \
+ *     9  20
+ *       /  \
+ *      15   7
  * return its depth = 3.
  */
 public class Solution {
     public int maxDepth(TreeNode root) {
         if (root == null) return 0;
-
         else return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 }
