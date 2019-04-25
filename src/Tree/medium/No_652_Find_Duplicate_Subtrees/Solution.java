0a5adba8b6d5e9c14a36306f19045dcab6a06e5d
lijunlong <1056407060@qq.com>
2019-04-25 19:58:37 +0800

    -----04/25----- 1、Find Duplicate Subtrees

--- src/Tree/medium/No_652_Find_Duplicate_Subtrees/Solution.java
+++ src/Tree/medium/No_652_Find_Duplicate_Subtrees/Solution.java
@@ -3,7 +3,9 @@ package Tree.medium.No_652_Find_Duplicate_Subtrees;
 import Tree.TreeNode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * FileName: Solution
@@ -37,7 +39,24 @@ import java.util.List;
  * Difficulty: Medium
  */
 public class Solution {
+    //序列化
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-        return new ArrayList<TreeNode>();
+        List<TreeNode> resArray = new ArrayList<>();
+        if (root == null) return resArray;
+        Map<String,List<TreeNode>> map = new HashMap<>();
+        handleNodeToString(root,map);
+        for (List<TreeNode> nodes:map.values()){
+            if (nodes.size()>1) resArray.add(nodes.get(0));
+        }
+        return resArray;
+    }
+
+    //将每一个左右节点的值都保存 成一个字符串 作为Key
+    private String handleNodeToString(TreeNode root, Map<String, List<TreeNode>> map) {
+        if (root == null) return "";
+        String s = "<" + handleNodeToString(root.left,map) + root.val + handleNodeToString(root.right,map) + ">";
+        if (!map.containsKey(s)) map.put(s,new ArrayList<>());
+        map.get(s).add(root);
+        return s;
     }
 }
