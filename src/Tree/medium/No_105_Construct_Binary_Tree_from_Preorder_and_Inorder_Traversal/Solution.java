d3a83df962eeb01ea938f2aa84b9b420e149be58
EdisonLi <18644056528@163.com>
2019-04-29 15:36:46 +0800

    -----04/29----- 1、Construct Binary Tree from Preorder and Inorder Traversal

---
+++ src/Tree/medium/No_105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal/Solution.java
@@ -0,0 +1,34 @@
+package Tree.medium.No_105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal;
+
+import Tree.TreeNode;
+
+/**
+ * FileName: Solution
+ * Author:   EdisonLi的Windows
+ * Date:     2019/4/29 15:01
+ * Description:
+ *
+ * Given preorder and inorder traversal of a tree, construct the binary tree.
+ *
+ * Note:
+ * You may assume that duplicates do not exist in the tree.
+ *
+ * For example, given
+ *
+ * preorder = [3,9,20,15,7]
+ * inorder = [9,3,15,20,7]
+ * Return the following binary tree:
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *
+ * Difficulty:
+ */
+public class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return new TreeNode(0);
+    }
+}
