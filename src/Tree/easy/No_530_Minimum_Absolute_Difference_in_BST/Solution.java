f277e860b04f6cffcb9732a9a442caab56b0f0dd
lijunlong <1056407060@qq.com>
2019-05-20 22:31:34 +0800

    -----05/20----- 1、Minimum Absolute Difference in BST

--- src/Tree/easy/No_530_Minimum_Absolute_Difference_in_BST/Solution.java
+++ src/Tree/easy/No_530_Minimum_Absolute_Difference_in_BST/Solution.java
@@ -9,20 +9,20 @@ import Tree.TreeNode;
  * Date:     2019-05-20 22:08
  * Description: Minimum Absolute Difference in BST
  * Given a binary search tree with non-negative values, find the minimum absolute difference between values of any two nodes.
- * <p>
+ *
  * Example:
- * <p>
+ *
  * Input:
- * <p>
- * 1
- * \
- * 3
- * /
- * 2
- * <p>
+ *
+ *    1
+ *     \
+ *      3
+ *     /
+ *    2
+ *
  * Output:
  * 1
- * <p>
+ *
  * Explanation:
  * The minimum absolute difference is 1, which is the difference between 2 and 1 (or between 2 and 3).
  */
