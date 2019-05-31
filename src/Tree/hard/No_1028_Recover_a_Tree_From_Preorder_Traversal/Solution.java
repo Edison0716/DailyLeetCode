de3ec6bac198375dbe06aba0b183097f9c306656
lijunlong <1056407060@qq.com>
2019-05-31 19:54:08 +0800

    -----05/31----- 1、No.1028 Recover a Tree From Preorder Traversal

--- src/Tree/hard/No_1028_Recover_a_Tree_From_Preorder_Traversal/Solution.java
+++ src/Tree/hard/No_1028_Recover_a_Tree_From_Preorder_Traversal/Solution.java
@@ -2,6 +2,8 @@ package Tree.hard.No_1028_Recover_a_Tree_From_Preorder_Traversal;
 
 import Tree.TreeNode;
 
+import java.util.Stack;
+
 /**
  * FileName: Solution
  * Author:   EdisonLi的Windows
@@ -41,6 +43,31 @@ import Tree.TreeNode;
  */
 public class Solution {
     public TreeNode recoverFromPreorder(String S) {
-        return null;
+        int level, val;
+        Stack<TreeNode> stack = new Stack<>();
+        for (int i = 0; i < S.length();) {
+            for (level = 0; S.charAt(i) == '-'; i++) {
+                level++;
+            }
+            for (val = 0; i < S.length() && S.charAt(i) != '-'; i++) {
+                val = val * 10 + (S.charAt(i) - '0');
+            }
+            while (stack.size() > level) {
+                stack.pop();
+            }
+            TreeNode node = new TreeNode(val);
+            if (!stack.isEmpty()) {
+                if (stack.peek().left == null) {
+                    stack.peek().left = node;
+                } else {
+                    stack.peek().right = node;
+                }
+            }
+            stack.add(node);
+        }
+        while (stack.size() > 1) {
+            stack.pop();
+        }
+        return stack.pop();
     }
 }
