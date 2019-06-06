ad37c5d5f1f84adb2d11fa8e4dfd21fd0c27e71a
EdisonLi <18644056528@163.com>
2019-06-06 12:42:04 +0800

    -----06/06----- 1、补06/05 No.82 Remove Duplicates from Sorted List II

--- src/LinkedList/No_82_Remove_Duplicates_from_Sorted_List_II/Solution.java
+++ src/LinkedList/No_82_Remove_Duplicates_from_Sorted_List_II/Solution.java
@@ -8,19 +8,44 @@ import LinkedList.ListNode;
  * Date:     2019/6/5 17:35
  * Description: No.82 Remove Duplicates from Sorted List II
  * Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: 1->2->3->3->4->4->5
  * Output: 1->2->5
  * Example 2:
- *
+ * <p>
  * Input: 1->1->1->2->3
  * Output: 2->3
  * Difficulty: Medium
  */
 public class Solution {
-    //双指针
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode start = new ListNode(0);
+        start.next = head;
+        //之前的节点用来判断第一个重复的节点的前一个节点
+        ListNode preNode = start;
+        //移动的节点
+        ListNode curNode = head;
+        while (curNode != null) {
+            //如果下一个节点不为空 并且 当前节点 等于下个节点
+            if (curNode.next != null && curNode.val == curNode.next.val) {
+                do {
+                    curNode = curNode.next;
+                }while (curNode.next != null && curNode.val == curNode.next.val);
+                //删除相等的节点也就是越过
+                preNode.next = curNode.next;
+            }else {
+                //不相等则移动 preNode
+                preNode = curNode;
+            }
+            curNode = curNode.next;
+        }
+        return start.next;
+    }
+
+    //双指针 行不通
 //    public ListNode deleteDuplicates(ListNode head) {
 //        ListNode dummyHead = head;
 //        ListNode fastP = dummyHead;
